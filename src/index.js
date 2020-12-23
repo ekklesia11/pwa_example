8e5c4b135ca9cf8e36c2865a85de742ae383fbce
DanielPark <happyyquokka@gmail.com>
2020-12-23 17:41:25 +0900

    service worker file created

--- src/index.js
+++ src/index.js
@@ -15,3 +15,12 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if('serviceWorker' in navigator){
+  try {
+    navigator.serviceWorker.register('serviceWorker.js');
+    console.log("Service Worker Registered");
+  } catch (error) {
+    console.log("Service Worker Registration Failed");
+  }
+}
